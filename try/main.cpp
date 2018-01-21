2f8492d9d3784719fb1f6605bfab1cca8e092a56
gifti99 <35659361+gifti99@users.noreply.github.com>
2018-01-21 17:47:56 -0500

    Initial Commit

---
+++ try/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  try
+//
+//  Created by gifti on 1/21/18.
+//  Copyright © 2018 gifti. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
